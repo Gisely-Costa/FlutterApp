02334ef6bc6695ec1ab64868e1b933a7d4f90093
giselymatematica <giselymatematica@gmail.com>
2026-05-13 19:52:49 +0000

    Adicionando arquivo theme.dart e atualizando main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'components.dart';
+import 'theme.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -30,8 +32,11 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: MaterialTheme.lightScheme(),
       ),
+      darkTheme: ThemeData(
+        colorScheme: MaterialTheme.lightMediumContrastScheme(),
+      ) ,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
